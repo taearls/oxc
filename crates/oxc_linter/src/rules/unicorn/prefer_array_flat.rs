b577efc2ca4e70663722e70093890a706e8ea688
Mikhail Baev <baev2m@gmail.com>
2026-04-13 13:15:33 +0500

    fix(linter/unicorn): handle optional chaining in `prefer-array-flat` and `no-invalid-remove-event-listener` (#21299)
    fixes commented out tests with optionals in in `unicorn/prefer-array-flat` and `unicorn/no-invalid-remove-event-listener` rules
    
    ---------
    
    Co-authored-by: Cameron <cameron.clark@hey.com>

--- crates/oxc_linter/src/rules/unicorn/prefer_array_flat.rs
+++ crates/oxc_linter/src/rules/unicorn/prefer_array_flat.rs
@@ -244,16 +244,24 @@ fn check_array_reduce_case<'a>(call_expr: &CallExpression<'a>, ctx: &LintContext
 // `[].concat(maybeArray)`
 // `[].concat(...array)`
 fn check_array_concat_case<'a>(call_expr: &CallExpression<'a>, ctx: &LintContext<'a>) {
-    if is_method_call(call_expr, None, Some(&["concat"]), Some(1), Some(1)) {
-        // `array.concat(maybeArray)`
-        if let Expression::ArrayExpression(array_expr) =
-            call_expr.callee.get_member_expr().unwrap().object()
-        {
-            if !array_expr.elements.is_empty() {
-                return;
-            }
-            ctx.diagnostic(prefer_array_flat_diagnostic(call_expr.span));
+    if call_expr.optional || !is_method_call(call_expr, None, Some(&["concat"]), Some(1), Some(1)) {
+        return;
+    }
+
+    let Some(member_expr) = call_expr.callee.as_member_expression() else {
+        return;
+    };
+
+    if member_expr.optional() {
+        return;
+    }
+
+    // `array.concat(maybeArray)`
+    if let Expression::ArrayExpression(array_expr) = member_expr.object() {
+        if !array_expr.elements.is_empty() {
+            return;
         }
+        ctx.diagnostic(prefer_array_flat_diagnostic(call_expr.span));
     }
 }
 
@@ -264,6 +272,10 @@ fn check_array_prototype_concat_case<'a>(call_expr: &CallExpression<'a>, ctx: &L
     let Some(member_expr) = call_expr.callee.get_member_expr() else {
         return;
     };
+
+    if call_expr.optional || member_expr.optional() {
+        return;
+    }
 
     if let Some(member_expr_obj) = member_expr.object().as_member_expression() {
         let is_call_call = is_method_call(call_expr, None, Some(&["call"]), Some(2), Some(2));
@@ -365,8 +377,8 @@ fn test() {
         "({}).concat(array)",
         "[].concat()",
         "[].concat(array, EXTRA_ARGUMENT)",
-        // "[]?.concat(array)",
-        // "[].concat?.(array)",
+        "[]?.concat(array)",
+        "[].concat?.(array)",
         "new [].concat(...array)",
         "[][concat](...array)",
         "[].notConcat(...array)",
@@ -374,8 +386,8 @@ fn test() {
         "({}).concat(...array)",
         "[].concat()",
         "[].concat(...array, EXTRA_ARGUMENT)",
-        // "[]?.concat(...array)",
-        // "[].concat?.(...array)",
+        "[]?.concat(...array)",
+        "[].concat?.(...array)",
         "new [].concat.apply([], array)",
         "[].concat.apply",
         "[].concat.apply([], ...array)",
@@ -388,8 +400,8 @@ fn test() {
         "[][concat].apply([], array)",
         "[].concat.notApply([], array)",
         "[].notConcat.apply([], array)",
-        // "[].concat.apply?.([], array)",
-        // "[].concat?.apply([], array)",
+        "[].concat.apply?.([], array)",
+        "[].concat?.apply([], array)",
         "[]?.concat.apply([], array)",
         "new Array.prototype.concat.apply([], array)",
         "Array.prototype.concat.apply",
@@ -405,8 +417,8 @@ fn test() {
         "Array.prototype.notConcat.apply([], array)",
         "Array.notPrototype.concat.apply([], array)",
         "NotArray.prototype.concat.apply([], array)",
-        // "Array.prototype.concat.apply?.([], array)",
-        // "Array.prototype.concat?.apply([], array)",
+        "Array.prototype.concat.apply?.([], array)",
+        "Array.prototype.concat?.apply([], array)",
         "Array.prototype?.concat.apply([], array)",
         "Array?.prototype.concat.apply([], array)",
         "object.Array.prototype.concat.apply([], array)",
